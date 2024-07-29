6a629d981b234fa224c71041eba5fab89e9f350d
GagoilKim <cplkimdh@gmail.com>
2024-07-29 22:39:18 +0900

    Initial Commit

---
+++ SampleCodes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleCodes
+//
+//  Created by Kyle Kim on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleCodes/SampleCodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCodesApp.swift
+//  SampleCodes
+//
+//  Created by Kyle Kim on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleCodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
